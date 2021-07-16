6cd2c5e692d727d13cc9169a7e3dd2da284c3afd
mikronco <ronco.michele@yahoo.it>
2021-07-16 12:35:32 +0200

    added plots y vs some of the Xs

--- random_data.py
+++ random_data.py
@@ -6,11 +6,16 @@ Created on Wed Jun 30 10:57:01 2021
 """
 
 import numpy as np 
-
+import matplotlib.pyplot as plt
 
 np.random.seed(10) 
 
 N = 100
 X = np.random.rand(N,5)
 y = 3*X[:,3] + 6*X[:,1] + np.random.normal(0,0.5,N) + 5
+
+plt.scatter(X[:,3], y)
+plt.scatter(X[:,1], y)
+plt.scatter(X[:,0], y)
+plt.show()
 
